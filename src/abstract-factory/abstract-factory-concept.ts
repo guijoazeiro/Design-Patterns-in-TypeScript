7f51caedc8907fc3108e00e986761e89054aba4f
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-02-21 16:20:29 -0300

    Abstract factory pattern

---
+++ src/abstract-factory/abstract-factory-concept.ts
@@ -0,0 +1,26 @@
+import { FactoryA, IProductA } from './factory-a'
+import { FactoryB, IProductB } from './factory-b'
+
+interface IProduct extends IProductA, IProductB { }
+
+class AbstractFactory {
+    static createObject(factory: string): IProduct | undefined {
+        try {
+            if (['aa', 'ab', 'ac'].indexOf(factory) > -1) {
+                return FactoryA.getObject(factory[1])
+            }
+            if (['ba', 'bb', 'bc'].indexOf(factory) > -1) {
+                return FactoryB.getObject(factory[1])
+            }
+            throw new Error('No Factory Found')
+        } catch (e) {
+            console.log(e)
+        }
+    }
+}
+
+let PRODUCT = AbstractFactory.createObject('ab')
+console.log(PRODUCT)
+
+PRODUCT = AbstractFactory.createObject('bc')
+console.log(PRODUCT)
