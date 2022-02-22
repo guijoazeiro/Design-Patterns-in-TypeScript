7b2ffcb09b04296128123a0bb2e2d01ad14080a0
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-02-22 16:42:56 -0300

    Singleton pattern

---
+++ src/singleton/singleton-concept.ts
@@ -0,0 +1,20 @@
+export class Singleton {
+    static instance: Singleton
+    id: number
+
+    constructor(id: number) {
+        this.id = id
+        if (Singleton.instance) {
+            return Singleton.instance
+        }
+        Singleton.instance = this
+    }
+}
+
+
+const OBJECT1 = new Singleton(1)
+const OBJECT2 = new Singleton(2)
+
+console.log(OBJECT1 === OBJECT2)
+console.log(OBJECT1.id)
+console.log(OBJECT2.id) 
