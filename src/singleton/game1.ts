95169a224e23b5429b5632a4bb78d120c974121f
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-02-23 15:29:44 -0300

    Singleton use case

---
+++ src/singleton/game1.ts
@@ -0,0 +1,14 @@
+import Leaderboard from './leaderboard'
+import Game from './igame'
+
+export class Game1 implements Game {
+    leaderboard: Leaderboard
+
+    constructor() {
+        this.leaderboard = new Leaderboard()
+    }
+
+    addWinner(position: number, name: string): void {
+        this.leaderboard.addWinner(position, name)
+    }
+}
