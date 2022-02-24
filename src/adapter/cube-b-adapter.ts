ffd19b628d89ce109169190471c840e99298176f
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-02-24 15:04:57 -0300

    Adapter use case

---
+++ src/adapter/cube-b-adapter.ts
@@ -0,0 +1,18 @@
+import { ICubeA } from './cube-a'
+import CubeB from './cube-b'
+
+export default class CubeBAdapter implements ICubeA {
+    #cube: CubeB
+
+    constructor() {
+        this.#cube = new CubeB()
+    }
+
+    manufacture(width: number, height: number, depth: number): boolean {
+        const success = this.#cube.create(
+            [0 - width / 2, 0 - height / 2, 0 - depth / 2],
+            [0 + width / 2, 0 + height / 2, 0 + depth / 2]
+        )
+        return success
+    }
+}
---
+++ src/adapter/cube-b.ts
@@ -0,0 +1,27 @@
+export interface ICubeB {
+    create(
+        top_left_front: [number, number, number],
+        bottom_right_back: [number, number, number]
+    ): boolean
+}
+
+export default class CubeB implements ICubeB {
+    static last_time = Date.now()
+
+    create(
+        top_left_front: [number, number, number],
+        bottom_right_back: [number, number, number]
+    ): boolean {
+        // if not busy, then manufacture a cube with coords
+        const now = Date.now()
+        if (now > CubeB.last_time + 3000) {
+            console.log(
+                `Company B built Cube with coords [${top_left_front[0]},${top_left_front[1]},${top_left_front[2]}],[${bottom_right_back[0]},${bottom_right_back[1]},${bottom_right_back[2]}]`
+            )
+            CubeB.last_time = now
+            return true
+        } else {
+            return false // busy
+        }
+    }
+}
