e8ce8ee0df6439759c72273b4c6b1f126e4ac827
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-02-28 17:38:57 -0300

    Proxy use case

---
+++ src/proxy/client.ts
@@ -0,0 +1,16 @@
+import Lion from './lion'
+
+const PROTEUS = new Lion()
+PROTEUS.tellMeYourForm()
+PROTEUS.tellMeTheFuture()
+PROTEUS.tellMeYourForm()
+PROTEUS.tellMeTheFuture()
+PROTEUS.tellMeYourForm()
+PROTEUS.tellMeTheFuture()
+PROTEUS.tellMeYourForm()
+PROTEUS.tellMeTheFuture()
+PROTEUS.tellMeYourForm()
+PROTEUS.tellMeTheFuture()
+PROTEUS.tellMeYourForm()
+PROTEUS.tellMeTheFuture()
+PROTEUS.tellMeYourForm()
