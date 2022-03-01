41f56c1f8b0dba979857c9e5c6d0071ac7202453
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-01 16:47:39 -0300

    Command use case

---
+++ src/command/light.ts
@@ -0,0 +1,11 @@
+export default class Light {
+    turnOn(): void {
+        // A set of instructions to run
+        console.log('Light turned ON')
+    }
+
+    turnOff(): void {
+        // A set of instructions to run
+        console.log('Light turned OFF')
+    }
+}
