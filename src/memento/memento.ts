06fde9b6d9812b177d3582c1a2f2f3360b52bfc5
Guilherme Joazeiro <guilhermejoazeiro@gmail.com>
2022-03-07 17:22:53 -0300

    Memento use case

---
+++ src/memento/memento.ts
@@ -0,0 +1,18 @@
+export default class Memento {
+    score: number
+    inventory: Set<string>
+    level: number
+    location: { x: number; y: number; z: number }
+
+    constructor(
+        score: number,
+        inventory: Set<string>,
+        level: number,
+        location: { x: number; y: number; z: number }
+    ) {
+        this.score = score
+        this.inventory = inventory
+        this.level = level
+        this.location = location
+    }
+}
